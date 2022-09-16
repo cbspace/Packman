599e37782b08ea7e10d121b5927cd0a9a278cc71
cbspace <24204459+cbspace@users.noreply.github.com>
2022-09-16 10:53:18 +1000

    Tidy up Error Class

--- src/lib/Error.h
+++ src/lib/Error.h
@@ -4,13 +4,27 @@
 
 using std::string;
 
-namespace Packman {
+namespace CBLib {
 
 class Error {
     public:
-        Error(const string &error_string = "Undefined Error");
-        Error(const string &error_string, const string &error_additional);
-        string get_error_string();
+        Error(const string &error_string = "Undefined Error") {
+            this->error_text = "Error: " + error_string;
+        }
+
+        Error(const string &error_string, const string &error_additional) {
+            this->error_text = error_string;
+            this->error_additional_text = error_additional;
+        }
+
+        string get_error_string() {
+            if (error_additional_text.empty()) {
+                return error_text;
+            } else { 
+                return error_text + " (" + error_additional_text + ")";
+            }
+        }
+        
     private:
         string error_text;
         string error_additional_text;
@@ -18,4 +32,4 @@ class Error {
 
 }
 
-using namespace Packman;
+using CBLib::Error;
